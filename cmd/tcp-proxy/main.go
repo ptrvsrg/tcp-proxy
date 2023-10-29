0e590fb782a21da74d641deabf484c94a85f6b66
ptrvsrg <s.petrov1@g.nsu.ru>
2023-10-30 03:25:36 +0700

    Add title

--- cmd/tcp-proxy/main.go
+++ cmd/tcp-proxy/main.go
@@ -7,7 +7,16 @@ import (
 	"tcp-proxy/internal/proxy"
 )
 
+const title string = "  ________________                                \n" +
+	" /_  __/ ____/ __ \\   ____  _________  _  ____  __\n" +
+	"  / / / /   / /_/ /  / __ \\/ ___/ __ \\| |/_/ / / /\n" +
+	" / / / /___/ ____/  / /_/ / /  / /_/ />  </ /_/ / \n" +
+	"/_/  \\____/_/      / .___/_/   \\____/_/|_|\\__, /  \n" +
+	"                  /_/                    /____/   \n" +
+	"tcp-proxy:1.0.0                                   \n"
+
 func main() {
+	println(title)
 	runtime.GOMAXPROCS(1)
 	port := cli.Parse()
 	proxy.Start(port)
